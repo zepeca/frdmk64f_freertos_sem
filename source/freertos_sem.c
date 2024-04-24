c89c2e83abf475631da38412fb3f459d7edb943d
zepeca <carlosa.zepeda@iteso.mx>
2024-04-23 21:45:06 -0600

    implementation 1
    circular buffer implemented

--- source/freertos_sem.c
+++ source/freertos_sem.c
@@ -26,17 +26,35 @@
 
 #define TASK_PRIO          (configMAX_PRIORITIES - 1)
 #define CONSUMER_LINE_SIZE 3
-SemaphoreHandle_t xSemaphore_producer;
-SemaphoreHandle_t xSemaphore_consumer;
+
+#define SIZE_RING_BUFF 5
+#define SIZE_RING_BUFF_DATA 11
+
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
-static void producer_task(void *pvParameters);
-static void consumer_task(void *pvParameters);
+static void producer_task(void);
+static void consumer_task(void);
+void ring_buff_write(void);
+void ring_buff_read(void);
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+char ring_buff[SIZE_RING_BUFF];
+uint8_t consumer_counter;
+uint8_t producer_counter;
+bool data_complete = false;
+char data_array[] = {'I', 'T', 'E', 'S', 'O', ' ', 'R', 'U', 'L', 'E', 'S'};
+uint8_t index_data_array;
+
+SemaphoreHandle_t xSemaphore_producer;
+SemaphoreHandle_t xSemaphore_consumer;
 
 /*******************************************************************************
  * Code
  ******************************************************************************/
+
 /*!
  * @brief Main function
  */
@@ -46,6 +64,7 @@ int main(void)
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
+    
     if (xTaskCreate(producer_task, "PRODUCER_TASK", configMINIMAL_STACK_SIZE + 128, NULL, TASK_PRIO, NULL) != pdPASS)
     {
         PRINTF("Task creation failed!.\r\n");
@@ -61,73 +80,83 @@ int main(void)
 /*!
  * @brief Task producer_task.
  */
-static void producer_task(void *pvParameters)
+static void producer_task(void)
 {
-    uint32_t i;
-
     PRINTF("Producer_task created.\r\n");
-    xSemaphore_producer = xSemaphoreCreateBinary();
+
+    xSemaphore_producer = xSemaphoreCreateCounting(SIZE_RING_BUFF, SIZE_RING_BUFF);
     if (xSemaphore_producer == NULL)
     {
         PRINTF("xSemaphore_producer creation failed.\r\n");
         vTaskSuspend(NULL);
     }
 
-    xSemaphore_consumer = xSemaphoreCreateBinary();
+    xSemaphore_consumer = xSemaphoreCreateCounting(SIZE_RING_BUFF,0);
     if (xSemaphore_consumer == NULL)
     {
         PRINTF("xSemaphore_consumer creation failed.\r\n");
         vTaskSuspend(NULL);
     }
 
-    for (i = 0; i < CONSUMER_LINE_SIZE; i++)
+    if (xTaskCreate(consumer_task, "CONSUMER_TASK", configMINIMAL_STACK_SIZE + 128, /*(void *)i*/ 0, TASK_PRIO, NULL) !=
+        pdPASS)
     {
-        if (xTaskCreate(consumer_task, "CONSUMER_TASK", configMINIMAL_STACK_SIZE + 128, (void *)i, TASK_PRIO, NULL) !=
-            pdPASS)
-        {
-            PRINTF("Task creation failed!.\r\n");
-            vTaskSuspend(NULL);
-        }
-        else
-        {
-            PRINTF("Consumer_task %d created.\r\n", i);
-        }
+        PRINTF("Task creation failed!.\r\n");
+        vTaskSuspend(NULL);
+    }
+    else
+    {
+        PRINTF("Consumer_task created.\r\n"/*, i*/);
     }
 
-    while (1)
-    {
-        /* Producer is ready to provide item. */
+    while (!data_complete) {
         xSemaphoreGive(xSemaphore_consumer);
-        /* Producer is waiting when consumer will be ready to accept item. */
-        if (xSemaphoreTake(xSemaphore_producer, portMAX_DELAY) == pdTRUE)
-        {
-            PRINTF("Producer released item.\r\n");
-        }
-        else
-        {
-            PRINTF("Producer is waiting for customer.\r\n");
+        if (xSemaphoreTake(xSemaphore_producer, portMAX_DELAY) == pdTRUE) {
+            ring_buff_write();
         }
     }
+    PRINTF("Producer has completed writing data.\r\n");
+    vTaskDelete(NULL);
 }
 
 /*!
  * @brief Task consumer_task.
  */
-static void consumer_task(void *pvParameters)
+static void consumer_task(void )
 {
-    PRINTF("Consumer number: %d\r\n", pvParameters);
-    while (1)
-    {
-        /* Consumer is ready to accept. */
+    while (!data_complete) {
         xSemaphoreGive(xSemaphore_producer);
-        /* Consumer is waiting when producer will be ready to produce item. */
-        if (xSemaphoreTake(xSemaphore_consumer, portMAX_DELAY) == pdTRUE)
-        {
-            PRINTF("Consumer %d accepted item.\r\n", pvParameters);
+        if (xSemaphoreTake(xSemaphore_consumer, portMAX_DELAY) == pdTRUE) {
+            ring_buff_read();
         }
-        else
-        {
-            PRINTF("Consumer %d is waiting for producer.\r\n", pvParameters);
-        }
+    }
+    PRINTF("\r\nConsumer has completed reading data.\r\n");
+    vTaskDelete(NULL);
+}
+
+void ring_buff_write() {
+    if (index_data_array < SIZE_RING_BUFF_DATA) {
+
+        //data array to ring buff
+        ring_buff[producer_counter] = data_array[index_data_array];
+
+        //update the prducer counter
+        producer_counter = (producer_counter + 1) % SIZE_RING_BUFF;
+
+        //update data array index
+        index_data_array++;
+    }
+}
+
+void ring_buff_read() {
+    
+    PRINTF("%c", ring_buff[consumer_counter]);
+
+    // Update the consumer index
+    consumer_counter = (consumer_counter + 1) % SIZE_RING_BUFF;
+
+    // Check if it's the end of the data array
+    if ( (SIZE_RING_BUFF_DATA == index_data_array) & (SIZE_RING_BUFF - 1 == consumer_counter)) {
+        data_complete = true;
     }
 }
